8163cae6c6a6356af6242bfd4eeb73dd71f10fbd
Chetan Viswa Kumar M <myneedichetan@gmail.com>
2026-01-11 12:42:04 +0530

    React Icons

--- src/components/WhatsAppButton.jsx
+++ src/components/WhatsAppButton.jsx
@@ -1,6 +1,6 @@
 // src/components/WhatsAppButton.jsx
 import React from 'react';
-import { FaWhatsapp } from "react-icons/fa6";
+import { FaWhatsapp } from "react-icons/fa";
 import { APP_CONFIG } from '../config/app';
 
 const WhatsAppButton = () => {
